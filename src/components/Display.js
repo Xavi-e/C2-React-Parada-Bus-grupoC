e5be7a9a601d04aff5d212ffcdb7bb99dfcbd315
Michaelsp11 <michaelsalgadop11@gmail.com>
2021-06-22 01:15:31 +0200

    Coger paradas de la API+bug mostrar paradas salia espacio+ocultar formtiempodelinea y tiempolinea

--- src/components/Display.js
+++ src/components/Display.js
@@ -1,65 +1,33 @@
-import { useCallback, useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 
-export const Display = () => {
+export const Display = (props) => {
+  const { paradas } = props;
   const [posicion, setPosicion] = useState(0);
   const [contador, setContador] = useState(0);
-  const paradasInventadas = [
-    {
-      status: "success",
-      data: {
-        ibus: [
-          {
-            routeId: "2271",
-            line: "V27",
-            "text-ca": "3 min",
-            "t-in-s": 190,
-            destination: "Pg. Marítim",
-            "t-in-min": 3,
-          },
-          {
-            routeId: "2270",
-            line: "V16",
-            "text-ca": "10min",
-            "t-in-s": 190,
-            destination: "Universitat",
-            "t-in-min": 10,
-          },
-          {
-            routeId: "2272",
-            line: "H12",
-            "text-ca": "1min",
-            "t-in-s": 190,
-            destination: "Pla de Palau",
-            "t-in-min": 1,
-          },
-        ],
-      },
-    },
-  ];
-  const {
-    data: { ibus },
-  } = paradasInventadas[0];
   useEffect(() => {
-    const interval = setInterval(() => {
-      if (contador <= paradasInventadas.length) {
-        setPosicion(posicion - 30);
-        setContador(contador + 1);
-      } else {
-        setPosicion(0);
-        setContador(0);
-      }
-    }, 2000);
-    return () => clearInterval(interval);
-  }, [contador, paradasInventadas.length, posicion]);
+    if (paradas.length > 1) {
+      const interval = setInterval(() => {
+        if (contador !== paradas.length - 1) {
+          setPosicion(posicion - 30);
+          setContador(contador + 1);
+        } else {
+          setPosicion(0);
+          setContador(0);
+        }
+      }, 2000);
+      return () => clearInterval(interval);
+    }
+  }, [contador, paradas.length, posicion]);
   return (
     <div className="display">
-      {ibus.map((parada) => (
-        <div key={parada.routeId} className="bus" style={{ top: posicion }}>
-          <span className="linea">{parada.line}</span>
-          <span className="destino">{parada.destination}</span>
-          <span className="tiempo">{parada["text-ca"]}</span>
-        </div>
-      ))}
+      {paradas.length > 0 &&
+        paradas.map((parada) => (
+          <div key={parada.routeId} className="bus" style={{ top: posicion }}>
+            <span className="linea">{parada.line}</span>
+            <span className="destino">{parada.destination}</span>
+            <span className="tiempo">{parada["text-ca"]}</span>
+          </div>
+        ))}
     </div>
   );
 };
--- src/components/FormNumeroParada.js
+++ src/components/FormNumeroParada.js
@@ -1,8 +1,20 @@
+import { useState } from "react";
+
 export const FormNumeroParada = (props) => {
+  const { getParadas } = props;
+  const [parada, setParada] = useState("");
+  const setDato = (evento) => {
+    setParada(evento.target.value);
+  };
   return (
-    <form>
+    <form
+      onSubmit={(evento) => {
+        evento.preventDefault();
+        getParadas(parada);
+      }}
+    >
       <label htmlFor="num-parada">Parada nº: </label>
-      <input type="number" id="num-parada" />
+      <input type="number" id="num-parada" onChange={setDato} />
       <button type="submit">Buscar</button>
     </form>
   );
